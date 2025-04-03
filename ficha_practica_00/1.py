9529168762efe94ad720b671f9b38b7724068b3e
aws11 <aws11@CESAEPRT.WAN>
2025-04-03 15:11:38 +0100

    pasta adicionada

---
+++ ficha_practica_00/1.py
@@ -0,0 +1,4 @@
+first_num = int(input("Insira um número:"))
+second_num= int(input("Insira um número:"))
+
+print("Soma:",first_num + second_num )
---
+++ ficha_practica_00/2.py
@@ -0,0 +1,8 @@
+first_num = int(input("Insira um número:"))
+second_num= int(input("Insira um número:"))
+
+print("Soma:",first_num + second_num )
+print("Divisão:",first_num / second_num )
+print("Multiplicação:",first_num * second_num )
+print("Subtração:",first_num - second_num )
+
